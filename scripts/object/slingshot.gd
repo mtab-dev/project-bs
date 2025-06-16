6e51c6af3a059f7c0a5d4370b46a75aa8f4344ab
mtab-dev <abranteselcio@gmail.com>
2025-06-15 23:08:09 -0300

    chore: godot 4.4

--- scripts/object/slingshot.gd
+++ scripts/object/slingshot.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 @onready var startShot: Marker2D = $gunPivot/texture/shots
 @onready var texture: AnimatedSprite2D = $gunPivot/texture
+@onready var gunPivot: Node2D = $gunPivot  # Pivô da arma
+@onready var player: Node2D = get_parent()  # O player é o nó pai da arma
+
 const BULLET = preload('res://scenes/objects/missile.tscn')
+
+# Distância fixa da arma em relação ao player
+const OFFSET_X = 20  # Ajuste conforme necessário
 
 func _ready() -> void:
 	texture.play('default')
@@ -20,13 +26,18 @@ func shoot(targetPosition: Vector2) -> void:
 	newBullet.position = startShot.global_position
 	newBullet.set_direction(newBullet.position, targetPosition)
 	get_tree().root.add_child(newBullet)
-	
-
 
 func _physics_process(delta: float) -> void:
-	rotate(get_angle_to(get_global_mouse_position()))
-	texture.rotation_degrees = -rotation_degrees
-
+	var mouse_x = get_global_mouse_position().x
+	var pivot_x = player.global_position.x
+	
+	# Determina se a arma deve ficar à direita ou à esquerda do player
+	if mouse_x >= pivot_x:
+		gunPivot.position.x = OFFSET_X  # Direita
+		texture.flip_h = false  # Mantém a sprite normal
+	else:
+		gunPivot.position.x = -OFFSET_X  # Esquerda
+		texture.flip_h = true  # Espelha a sprite para o lado esquerdo
 
 func _on_texture_animation_finished() -> void:
 	if texture.animation == 'shot':
